f44ffd550cd5c926a056745c96a480450fe19a9f
Spanioulis <pani77sps@gmail.com>
2024-10-16 11:26:24 +0200

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -7,7 +7,13 @@ class Person(models.Model):
     age = models.IntegerField()
     gender = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title + ' - ' + self.person.name
