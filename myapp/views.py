64f8e65fa075a600be68164edc9fbd0e536ebe7d
Spanioulis <pani77sps@gmail.com>
2024-10-16 11:37:30 +0200

    Create templates

--- myapp/views.py
+++ myapp/views.py
@@ -1,22 +1,24 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, JsonResponse
 from .models import Person, Task
 
 # Create your views here.
 
 def index(request):
-  return HttpResponse("<h1>Home Page!</h1>")
+  return render(request, 'index.html')
 
 def hello(request, username):
   return HttpResponse("<h1>Hola %s!</h1>" % username)
 
 def about(request):
-  return HttpResponse("<h1>About</h1>")
+  return render(request, 'about.html')
 
 def persons(request):
-  persons = list(Person.objects.values())
-  return JsonResponse(persons, safe=False)
+  # persons = list(Person.objects.values())
+  # return JsonResponse(persons, safe=False)
+  return render(request, 'persons.html')
 
 def tasks(request, id):
-  task = get_object_or_404(Task, id=id)
-  return HttpResponse('task: %s' % task.title)
+  # task = get_object_or_404(Task, id=id)
+  # return HttpResponse('task: %s' % task.title)
+  return render(request, 'tasks.html')
